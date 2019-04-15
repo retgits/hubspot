aa926ca172d7e59ea14f703db5688b0824f72b80
retgits <lstigter@gmail.com>
2019-04-14 17:44:51 -0700

    feat: added tickets api

---
+++ client/tickets/api.go
@@ -0,0 +1,26 @@
+// Package tickets is part of HubSpots preview program, and should be considered as a non-stable release that
+// will be subject to bugs and breaking changes while under development.
+package tickets
+
+import "encoding/json"
+
+type HubSpotTickets struct {
+	Objects []Object `json:"objects"`
+	HasMore bool     `json:"hasMore"`
+	Offset  int64    `json:"offset"`
+}
+
+type Object struct {
+	ObjectType string                            `json:"objectType"`
+	PortalID   int64                             `json:"portalId"`
+	ObjectID   int64                             `json:"objectId"`
+	Properties map[string]map[string]interface{} `json:"properties"`
+	Version    int64                             `json:"version"`
+	IsDeleted  bool                              `json:"isDeleted"`
+}
+
+func unmarshalTickets(data []byte) (HubSpotTickets, error) {
+	var r HubSpotTickets
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
---
+++ client/tickets/service.go
@@ -0,0 +1,104 @@
+// Package tickets is part of HubSpots preview program, and should be considered as a non-stable release that
+// will be subject to bugs and breaking changes while under development.
+package tickets
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/retgits/hubspot/client"
+)
+
+const (
+	defaultOffSet int64 = 0
+	// allTicketsEndpoint is the endpoint to retrieve all tickets
+	allTicketsEndpoint = "crm-objects/v1/objects/tickets/paged"
+)
+
+// Tickets contains the elements to communicate with the HubSpot Tickets endpoints.
+type Tickets struct {
+	*client.Client
+	OffSet     int64
+	Properties []string
+}
+
+// New creates a new instance of the Tickets service with default settings.
+func New(c *client.Client) *Tickets {
+	return &Tickets{
+		c, defaultOffSet, nil,
+	}
+}
+
+// WithOffSet sets an offset value returning a Tickets pointer for
+// chaining.
+func (t *Tickets) WithOffSet(offset int64) *Tickets {
+	t.OffSet = offset
+	return t
+}
+
+// WithProperties sets the "property" parameter, then the properties in the "ticket" object in the
+// returned data will only include the property or properties that you request. returning a Tickets
+// pointer for chaining.
+func (t *Tickets) WithProperties(props []string) *Tickets {
+	t.Properties = props
+	return t
+}
+
+// GetAllTickets gets all tickets from a portal. By default you will only get a few system fields for
+// any tickets in the response. If you want to get specific properties, you'll need to use the properties
+// parameter.
+func (t *Tickets) GetAllTickets() ([]Object, error) {
+	url := url(t, allTicketsEndpoint)
+
+	res, err := t.Call(url, http.MethodGet, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	temp, err := unmarshalTickets(res)
+	if err != nil {
+		return nil, err
+	}
+
+	t = t.WithOffSet(temp.Offset)
+	tickets := temp.Objects
+
+	if temp.HasMore {
+		for {
+			res, err := t.Call(url, http.MethodGet, nil)
+			if err != nil {
+				return nil, err
+			}
+
+			temp, err := unmarshalTickets(res)
+			if err != nil {
+				return nil, err
+			}
+
+			t = t.WithOffSet(temp.Offset)
+
+			tickets = append(tickets, temp.Objects...)
+			if !temp.HasMore {
+				break
+			}
+		}
+	}
+
+	return tickets, nil
+}
+
+// Construct the proper URL to call
+func url(t *Tickets, u string) string {
+	url := ""
+	url = fmt.Sprintf("%s?hapikey=%s", u, t.APIKey)
+
+	if t.OffSet > 0 {
+		url = fmt.Sprintf("%s&Offset=%d", url, t.OffSet)
+	}
+
+	for idx := range t.Properties {
+		url = fmt.Sprintf("%s&properties=%s", url, t.Properties[idx])
+	}
+
+	return url
+}
